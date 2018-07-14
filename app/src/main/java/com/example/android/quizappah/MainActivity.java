3c4f692e6e4f2f2d59a815103971388f06db1cd4
lehieu30794 <lehieu30794@gmail.com>
2018-07-13 21:56:55 -0600

    Change onClick to define id of some views

--- app/src/main/java/com/example/android/quizappah/MainActivity.java
+++ app/src/main/java/com/example/android/quizappah/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.quizappah;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//    Display score
+    public void displayScore(int score) {
+        TextView scoreTextView = (TextView) findViewById(R.id.score_text_view);
+        scoreTextView.setText("" + score);
+    }
+//    Display score when hit submit
+    public void submit (View view){
+        RadioButton isCheck
+        displayScore(10);
+    }
+//    Will need method calculating the number of correct answer
 }
